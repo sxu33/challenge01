c73b7fe001a2d9b0cddfc8157ca0cd305a04eac5
sxu <sxu@szx>
2024-05-21 23:35:44 -0500

    fix typo

--- challenge01.rb
+++ challenge01.rb
@@ -12,7 +12,7 @@ sagan = [
         ]
 
     # We are a way for the cosmos to know itself.
-puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
+puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
 
 
 ghosts = [
